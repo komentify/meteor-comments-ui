d24a246c8e5eeb269de47b7182978854e247b485
Matteo De Micheli <mat.demicheli@gmail.com>
2016-04-07 11:20:04 +0200

    Expose mediaService in Public API

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'arkham:comments-ui',
   summary: 'Simple templates for disqus-like comment functionality',
-  version: '1.0.1',
+  version: '1.0.2',
   git: 'https://github.com/ARKHAM-Enterprises/meteor-comments-ui.git'
 });
 
