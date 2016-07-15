3f1a32a8c4dc692b274f10117de1abce1eedc180
Matteo De Micheli <mat.demicheli@gmail.com>
2016-07-15 17:07:00 +0200

    Do not load anon user data from local storage

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'arkham:comments-ui',
   summary: 'Simple templates for disqus-like comment functionality',
-  version: '1.1.7',
+  version: '1.1.8',
   git: 'https://github.com/komentify/meteor-comments-ui.git'
 });
 
