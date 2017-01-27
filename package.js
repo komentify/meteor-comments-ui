b8d67f7365b7e0e95126829d6928b4a4bf451b30
Matteo De Micheli <matteodem@protonmail.com>
2017-01-27 09:18:10 +0100

    Fix duplicate anon usernames

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'arkham:comments-ui',
   summary: 'Simple templates for disqus-like comment functionality',
-  version: '1.3.1',
+  version: '1.3.2',
   git: 'https://github.com/komentify/meteor-comments-ui.git'
 });
 
